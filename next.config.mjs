2b5749d2cc1dcff40c3cf156a00308b6ecf22d1a
Adeeluddin Siddiqui <ad.siddiqui2@gmail.com>
2025-03-09 22:48:09 -0700

    Update site with Next.js build

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',  // Enable static exports
-  distDir: '../personal webs',  // Output directly to the GitHub Pages directory
   images: {
     unoptimized: true, // Required for static export
   },
   trailingSlash: true, // Recommended for static exports
-  assetPrefix: './',
 };
 
 export default nextConfig; 
